1f366d495f6cf373953d7d422dffe21e76fbec38
Saleha Khan <108184199+EngSaleha@users.noreply.github.com>
2025-05-02 18:23:30 +0300

    Add motor

--- maze.py
+++ maze.py
@@ -17,8 +17,8 @@ SERVO = 0
 TRIG = 11
 ECHO = 8
     # Motor
-ENA = 4
-ENB = 17
+Motor_A_EN    = 4
+Motor_B_EN    = 17
 Motor_A_Pin1  = 26
 Motor_A_Pin2  = 21
 Motor_B_Pin1  = 27
@@ -36,12 +36,12 @@ LED_DMA = 10 # DMA channel to use for generating signal (try 10)
 GPIO.setup(TRIG,GPIO.OUT) 
 GPIO.setup(ECHO,GPIO.IN)  
     # Motor
-GPIO.setup(ENA,GPIO.OUT)
-GPIO.setup(ENB,GPIO.OUT)
-GPIO.setup(IN1,GPIO.OUT)
-GPIO.setup(IN2,GPIO.OUT)
-GPIO.setup(IN3,GPIO.OUT)
-GPIO.setup(IN4,GPIO.OUT)
+GPIO.setup(Motor_A_EN, GPIO.OUT)
+GPIO.setup(Motor_B_EN, GPIO.OUT)
+GPIO.setup(Motor_A_Pin1, GPIO.OUT)
+GPIO.setup(Motor_A_Pin2, GPIO.OUT)
+GPIO.setup(Motor_B_Pin1, GPIO.OUT)
+GPIO.setup(Motor_B_Pin2, GPIO.OUT)
     # 
 
 # Define Constants and Objects
@@ -49,8 +49,14 @@ GPIO.setup(IN4,GPIO.OUT)
 speed_of_sound = 315
 
     # Motor
-pwm_A=GPIO.PWM(ENA,1000) # p is an object that gives a neat way to control the motor, 1000 Hz Freq.
-pwm_B=GPIO.PWM(ENB,1000) # p is an object that gives a neat way to control the motor, 1000 Hz Freq.
+Dir_forward   = 1
+Dir_backward  = 0
+left_forward  = 1
+left_backward = 0
+right_forward = 0
+right_backward= 1
+pwm_A = GPIO.PWM(Motor_A_EN, 1000)
+pwm_B = GPIO.PWM(Motor_B_EN, 1000)
 
     # LED strip configuration:
 LED_FREQ_HZ = 800000 # LED signal frequency in hertz (usually 800khz)
@@ -88,13 +94,89 @@ def servo_to_min():
     time.sleep(1)
 
     # Motor
-def motorStop(): 
+def motorStop():
 	GPIO.output(Motor_A_Pin1, GPIO.LOW)
 	GPIO.output(Motor_A_Pin2, GPIO.LOW)
 	GPIO.output(Motor_B_Pin1, GPIO.LOW)
 	GPIO.output(Motor_B_Pin2, GPIO.LOW)
-	GPIO.output(ENA, GPIO.LOW)
-	GPIO.output(ENB, GPIO.LOW)
+	GPIO.output(Motor_A_EN, GPIO.LOW)
+	GPIO.output(Motor_B_EN, GPIO.LOW)
+
+def motor_left(status, direction, speed):# Motor 2 positive and negative rotation
+	if status == 0: # stop
+		GPIO.output(Motor_B_Pin1, GPIO.LOW)
+		GPIO.output(Motor_B_Pin2, GPIO.LOW)
+		GPIO.output(Motor_B_EN, GPIO.LOW)
+	else:
+		if direction == Dir_backward:
+			GPIO.output(Motor_B_Pin1, GPIO.HIGH)
+			GPIO.output(Motor_B_Pin2, GPIO.LOW)
+			pwm_B.start(100)
+			pwm_B.ChangeDutyCycle(speed)
+		elif direction == Dir_forward:
+			GPIO.output(Motor_B_Pin1, GPIO.LOW)
+			GPIO.output(Motor_B_Pin2, GPIO.HIGH)
+			pwm_B.start(0)
+			pwm_B.ChangeDutyCycle(speed)
+
+
+def motor_right(status, direction, speed): # Motor 1 positive and negative rotation
+	if status == 0: # stop
+		GPIO.output(Motor_A_Pin1, GPIO.LOW)
+		GPIO.output(Motor_A_Pin2, GPIO.LOW)
+		GPIO.output(Motor_A_EN, GPIO.LOW)
+	else:
+		if direction == Dir_forward:#
+			GPIO.output(Motor_A_Pin1, GPIO.HIGH)
+			GPIO.output(Motor_A_Pin2, GPIO.LOW)
+			pwm_A.start(100)
+			pwm_A.ChangeDutyCycle(speed)
+		elif direction == Dir_backward:
+			GPIO.output(Motor_A_Pin1, GPIO.LOW)
+			GPIO.output(Motor_A_Pin2, GPIO.HIGH)
+			pwm_A.start(0)
+			pwm_A.ChangeDutyCycle(speed)
+	return direction
+
+
+def move(speed, direction, turn, radius=0.6):   # 0 < radius <= 1  
+	#speed = 100
+	if direction == 'forward':
+		if turn == 'right':
+			motor_left(0, left_backward, int(speed*radius))
+			motor_right(1, right_forward, speed)
+		elif turn == 'left':
+			motor_left(1, left_forward, speed)
+			motor_right(0, right_backward, int(speed*radius))
+		else:
+			motor_left(1, left_forward, speed)
+			motor_right(1, right_forward, speed)
+	elif direction == 'backward':
+		if turn == 'right':
+			motor_left(0, left_forward, int(speed*radius))
+			motor_right(1, right_backward, speed)
+		elif turn == 'left':
+			motor_left(1, left_backward, speed)
+			motor_right(0, right_forward, int(speed*radius))
+		else:
+			motor_left(1, left_backward, speed)
+			motor_right(1, right_backward, speed)
+	elif direction == 'no':
+		if turn == 'right':
+			motor_left(1, left_backward, speed)
+			motor_right(1, right_forward, speed)
+		elif turn == 'left':
+			motor_left(1, left_forward, speed)
+			motor_right(1, right_backward, speed)
+		else:
+			motorStop()
+	else:
+		pass
+
+def destroy():
+	motorStop()
+	GPIO.cleanup()             # Release resource
+	
 
 
 
